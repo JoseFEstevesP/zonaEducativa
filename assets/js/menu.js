d4c5b421c3ea44d9ddd8a4d35f0e93fb19f69b67
gato-99 <59174114+gato-99@users.noreply.github.com>
2021-03-21 00:44:55 -0400

    solucion minima del menu que no se visualizo en su momento

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,12 +4,3 @@ const menu = document.getElementById("menu");
 btnIcon.addEventListener("click", () => {
   menu.classList.toggle("menu--open_menu");
 });
-// menu submenu
-const main_menu = document.getElementById("main_menu");
-const subMenu = document.getElementById("sub-menu");
-main_menu.addEventListener("click", (e) => {
-  if (e.target.classList.contains("open-sub-menu")) {
-    console.log();
-    // subMenu.classList.toggle("menu__subMenu--subMenu-open");
-  }
-});
