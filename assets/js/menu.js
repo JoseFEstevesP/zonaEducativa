21a69c5e4b3e5528050d16cb4d758f397e135ab4
jose Esteves <josefelixestevenpacheco@gmail.com>
2021-04-01 20:20:11 -0400

    subir 1

--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-const btnIcon = document.getElementById("btn-icon");
-const menu = document.getElementById("menu");
-
-btnIcon.addEventListener("click", () => {
-  menu.classList.toggle("menu--open_menu");
-});
--- views/Errors/error.php
+++
@@ -1,4 +0,0 @@
-<?php include("views/include/header.php") ?>
-
-  <h1>pagina no encontrada</h1>
-<?php include("views/include/footer.php") ?>
